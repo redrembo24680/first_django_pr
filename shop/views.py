478ce1903ab72ae39923eeef270bd23c4e6faeca
Roman <cisinskijroman@gmail.com>
2025-01-25 19:29:34 +0200

    First comit

---
+++ shop/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse, Http404
+from .models import Course
+# Create your views here.
+
+
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'shop/courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+    try:
+        course = Course.objects.get(pk=course_id)
+        return render(request, 'shop/single_course.html', {'course': course})
+    except Course.DoesNotExist:
+        raise Http404()
+    # course = get_list_or_404(Course, pk=course_id)
+    # return render(request, 'shop/single_course.html', {'course': course})
